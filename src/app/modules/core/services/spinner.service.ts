3f5ef207f5219146efc1b0b3f6ce0db855c2f961
Padrino43 <mkubis42@gmail.com>
2024-11-04 17:36:13 +0100

    added spinner

---
+++ src/app/modules/core/services/spinner.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SpinnerService {
+  isLoading = new BehaviorSubject<boolean>(false);
+
+  showSpinner() {
+    this.isLoading.next(true);
+  }
+  hide() {
+    this.isLoading.next(false);
+  }
+}
